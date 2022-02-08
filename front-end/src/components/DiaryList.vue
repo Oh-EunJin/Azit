13f3aec6e9e4daa5a95d261fed8cb5581f404b7a
ethankim93 <wsmh.kim12@gmail.com>
2022-02-08 16:02:32 +0900

    temp:git 정리용

--- front-end/src/components/DiaryList.vue
+++ front-end/src/components/DiaryList.vue
@@ -4,7 +4,7 @@
       <div class="flex md4 lg3">
         <diary-list-item/>
       </div>
-
+  c
       <!-- <div class="flex md4 lg3" v-if="diarys.length"> 
         <diary-list-item
         v-for="(diary, index) in diarys"
@@ -21,6 +21,7 @@
 
 <script>
 import DiaryListItem from '@/components/DiaryListItem.vue'
+import axios from "axios";
 
 
 export default {
@@ -30,12 +31,25 @@ export default {
 
   data(){
     return{
+      diaryList : []
     };
   },
   methods:{
     newdiary(){
       this.$router.push({ name : 'Record'})
-    }
+    },
+    async getList() {
+    this.diaryList = await this.$api("https://15f8ee3f-349a-4cf2-86ef-1f8c61ec2767.mock.pstmn.io/api/diarylist/1","get");
+    console.log(this.diaryList)
+  }
+  },
+  created() {
+    axios.get('https://15f8ee3f-349a-4cf2-86ef-1f8c61ec2767.mock.pstmn.io/api/diarylist/1')
+    .then(response => {
+      this.diarys = response.data
+      console.log(response.data)
+    })
+
   }
 };
 
